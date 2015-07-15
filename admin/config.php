f87f8432363f6a80753b0c99c54f06a5e8630f54
Cookie89 <mrsebcook@aol.com>
2015-07-15 19:52:49 +0100

    First commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://'.$_SERVER['SERVER_NAME'].':8080/opencart/admin/');
+define('HTTP_CATALOG', 'http://'.$_SERVER['SERVER_NAME'].':8080/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://'.$_SERVER['SERVER_NAME'].':8443/opencart/admin/');
+define('HTTPS_CATALOG', 'https://'.$_SERVER['SERVER_NAME'].':8443/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/admin/');
+define('DIR_SYSTEM', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/');
+define('DIR_LANGUAGE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/admin/language/');
+define('DIR_TEMPLATE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/admin/view/template/');
+define('DIR_CONFIG', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/config/');
+define('DIR_IMAGE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/image/');
+define('DIR_CACHE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/download/');
+define('DIR_UPLOAD', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/upload/');
+define('DIR_LOGS', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/logs/');
+define('DIR_MODIFICATION', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/modification/');
+define('DIR_CATALOG', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'bn_opencart');
+define('DB_PASSWORD', 'ad83a742fa');
+define('DB_DATABASE', 'bitnami_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://'.$_SERVER['SERVER_NAME'].':8080/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://'.$_SERVER['SERVER_NAME'].':8443/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/catalog/');
+define('DIR_SYSTEM', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/');
+define('DIR_LANGUAGE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/config/');
+define('DIR_IMAGE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/image/');
+define('DIR_CACHE', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/download/');
+define('DIR_UPLOAD', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/upload/');
+define('DIR_MODIFICATION', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/modification/');
+define('DIR_LOGS', '/Applications/opencart-2.0.3.1-0/apps/opencart/htdocs/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'bn_opencart');
+define('DB_PASSWORD', 'ad83a742fa');
+define('DB_DATABASE', 'bitnami_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
